14e80f1c0dea751850a2efe3414dafc93620b14a
54simple <fiftyfoursimple10@gmail.com>
2025-06-02 12:40:14 +0300

    sas

--- lib/authURL.ts
+++ lib/authURL.ts
@@ -16,7 +16,7 @@ export const prodGoogleURL = "https://liv-care.vercel.app/dashboard";
  * The Redirect Link for production or development
  */
 
-export const _redirectLink = process.env.NODE_ENV == "development" ? devGoogleURL : prodGoogleURL;
+export const _redirectLink =  prodGoogleURL;
 
 
-export const _redirectLink2 = process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://liv-care.vercel.app"
+export const _redirectLink2 = "https://liv-care.vercel.app"
